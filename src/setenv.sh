f1893a21d31be5d0e5b0416851d7fe8aeae093d6
Wangxiaobin123 <1114929951@qq.com>
2019-12-07 13:52:10 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=Wangxiaobin123
 
 export MYCLUSTER=tektonknativebeijing24
-export KUBECONFIG=
+export KUBECONFIG=/home/1.114929951e+09
 
 export NAMESPACE=shengbiin
 export EMAIL=1114929951@qq.com
