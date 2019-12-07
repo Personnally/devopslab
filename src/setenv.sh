56723b384b80f005f1e450bcec56d9f4f0ceb91f
Wangxiaobin123 <1114929951@qq.com>
2019-12-07 13:54:20 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=Wangxiaobin123
 
 export MYCLUSTER=tektonknativebeijing24
-export KUBECONFIG=/home/1.114929951e+09
+export KUBECONFIG=/tmp/ic/AC/.bluemix/plugins/container-service/clusters/tektonknativebeijing24/kube-config-tok04-tektonknativebeijing24.yml
 
 export NAMESPACE=shengbiin
 export EMAIL=1114929951@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing24-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
